1a57f748fddd28b518773bdc18943f556da0154a
king-David <davidojeata0@gmail.com>
2026-04-27 02:32:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
-export default defineConfig({
-  plugins: [react()],
+import { defineConfig } from 'vite' 
+import react from '@vitejs/plugin-react' 
+export default defineConfig({ plugins: [react()], 
 })
-3. tailwind.config.js(Create in the root folder)/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ], ki
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
